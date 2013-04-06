800b1ab260b673020e3bb6d39c6d4223caa273a6
valerav@gmail.com <valerav@gmail.com@1f6298ab-18d1-02c1-b26e-8805373bb203>
2013-04-06 20:46:17 +0000

    add sources

--- trunk/src/test/java/nl/clearj/commons/thread/BufferedProcessingThreadTest.java
+++ trunk/src/test/java/nl/clearj/commons/thread/BufferedProcessingThreadTest.java
@@ -1,13 +1,36 @@
 package nl.clearj.commons.thread;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class BufferedProcessingThreadTest {
 
+	private BufferedProcessingThread<Object, Object> bufferedProcessingThread;
+	private int processedCount;
+
 	@Before
 	public void setUp() throws Exception {
+		processedCount = 0;
+		bufferedProcessingThread = new BufferedProcessingThread<Object, Object>("test thread", true) {
+
+			@Override
+			void processValue(Object valueToProcess) {
+				Assert.assertNotNull(valueToProcess);
+				synchronized (bufferedProcessingThread) {
+					processedCount++;
+				}
+			}
+
+			@Override
+			void initializeProcessing() {
+			}
+
+			@Override
+			void finalizeProcessing() {
+			}
+		};
 	}
 
 	@After
@@ -15,6 +38,14 @@ public class BufferedProcessingThreadTest {
 	}
 
 	@Test
-	public void test() {
+	public void test() throws InterruptedException {
+		long start = System.currentTimeMillis();
+		for (int i = 1; i < (1000 * 10000); i++) {
+			bufferedProcessingThread.put(new Object(), new Object());
+		}
+		System.out.println(processedCount);
+		System.out.println(bufferedProcessingThread.size());
+		System.out.println(processedCount + bufferedProcessingThread.size());
+		System.out.println((System.currentTimeMillis() - start) + "ms");
 	}
 }
