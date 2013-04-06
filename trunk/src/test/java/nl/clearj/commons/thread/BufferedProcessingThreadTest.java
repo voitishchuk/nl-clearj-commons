44fbb2767921d6238f5a18eff18946514c7b8d88
valerav@gmail.com <valerav@gmail.com@1f6298ab-18d1-02c1-b26e-8805373bb203>
2013-04-06 20:34:24 +0000

    project structure

---
+++ trunk/src/test/java/nl/clearj/commons/thread/BufferedProcessingThreadTest.java
@@ -0,0 +1,20 @@
+package nl.clearj.commons.thread;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BufferedProcessingThreadTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+	}
+}
